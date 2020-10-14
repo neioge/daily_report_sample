2d8b63bed2b394f4db6f79f2bb2c3f63e6a9c15f
neioge <katanonaoto@gmail.com>
2020-10-14 16:29:02 +0900

    Add login / logout link

--- src/controllers/toppage/TopPageIndexServlet.java
+++ src/controllers/toppage/TopPageIndexServlet.java
@@ -31,7 +31,11 @@ public class TopPageIndexServlet extends HttpServlet {
          */
         protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-            //ビューをにフォワードするだけ
+            // ログインインデックスコントローラの下のほうに、ちゃんとフラッシュメッセージがあればビューに送るようにできている
+            if(request.getSession().getAttribute("flush") != null) {
+                request.setAttribute("flush", request.getSession().getAttribute("flush"));
+                request.getSession().removeAttribute("flush");
+            }
 
             RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/topPage/index.jsp");
             rd.forward(request, response);
