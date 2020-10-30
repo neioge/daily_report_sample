1410e68d9ad8a3056aa522bf583de6c729d591d3
neioge <katanonaoto@gmail.com>
2020-10-30 14:11:44 +0900

    Add Relationship class

---
+++ src/src/controllers/relationships/RelationshipsCreateServlet.java
@@ -0,0 +1,22 @@
+package src.controllers.relationships;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/relationships/create")
+public class RelationshipsCreateServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    public RelationshipsCreateServlet() {
+        super();
+    }
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	}
+
+}
---
+++ src/src/models/Relationship.java
@@ -0,0 +1,103 @@
+package src.models;
+
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+
+@Table(name = "relationships")
+
+@NamedQueries({
+    @NamedQuery(
+            name = "checkRelationship",
+            query = "SELECT r FROM Relationship AS r WHERE r.following = :followingEmployee AND r.followed = :following"
+    ),
+    @NamedQuery(
+            name = "getAllFollowing",
+            query = "SELECT r FROM Relationship AS r WHERE r.following = :following"
+    ),
+    @NamedQuery(
+            name = "getAllFollowed",
+            query = "SELECT r FROM Relationship AS r WHERE r.followed = :followed"
+    ),
+    @NamedQuery(
+            name = "getFollowingCount",
+            query = "SELECT COUNT(r) FROM Relationship AS r WHERE r.following = :following"
+    ),
+    @NamedQuery(
+            name = "getFollowedCount",
+            query = "SELECT COUNT(r) FROM Relationship AS r WHERE r.followed = :followed"
+    ),
+})
+
+@Entity
+public class Relationship {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "following_id", nullable = false)
+    private Employee following;
+
+    @ManyToOne
+    @JoinColumn(name = "followed_id", nullable = false)
+    private Employee followed;
+
+    @Column(name = "created_at", nullable = false)
+    private Timestamp created_at;
+
+    @Column(name = "updated_at", nullable = false)
+    private Timestamp updated_at;
+
+    // ここからgetterとsetter
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+
+    public Employee getFollowing() {
+        return following;
+    }
+
+    public void setFollowing(Employee following) {
+        this.following = following;
+    }
+
+    public Employee getFollowed() {
+        return followed;
+    }
+    public void setFollowed(Employee followed) {
+        this.followed = followed;
+    }
+
+    public Timestamp getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(Timestamp created_at) {
+        this.created_at = created_at;
+    }
+
+    public Timestamp getUpdated_at() {
+        return updated_at;
+    }
+
+    public void setUpdated_at(Timestamp updated_at) {
+        this.updated_at = updated_at;
+    }
+}
